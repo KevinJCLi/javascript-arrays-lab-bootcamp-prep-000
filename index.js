848dab2afc426db514e3a9181b94d344b2d6b279
Kevin Li <kevinjcli@hotmail.com>
2018-02-01 03:01:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,48 @@ function destructivelyAppendKitten (name) {
   kittens.push(name);
   return kittens;
 }
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten (name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten (name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten () {
+  kittens.slice(1,kittens.length - 1);
+}
+
+function removeFirstKitten () {
+  kittens.slice(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
